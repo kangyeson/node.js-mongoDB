71fa0451c4e8c6985c14e2721d7226db84d6e369
kangyeson <s2019w01@e-mirim.hs.kr>
2021-07-02 05:15:39 +0900

    Final Update

--- server.js
+++ server.js
@@ -4,18 +4,24 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv/config');
 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
+const EmployeeRoute = require('./routes/employee');
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 
 const app = express();
+
+app.use('/api/employee', EmployeeRoute);
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-db.on('error', (err) => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
+
+db.on('error', function (err){
     console.log(err);
 });
 
-db.once('open', () => {
-    console.log('디비 연결');
-});
+db.once('open', function (){
+    console.log('DB연결');
+})
